3718a8f226ed431e25ccebaf48984864cf13bfec
乔治 <18890176107@163.com>
2024-04-14 20:31:49 +0800

    添加redis配置

---
+++ quiz-main/src/main/java/com/quiz/config/RedisConfig.java
@@ -0,0 +1,35 @@
+package com.quiz.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * <p>
+ * RedisConfig
+ * </p>
+ *
+ * @author XGeorge
+ * @since 2024/4/14 18:44
+ */
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        redisTemplate.setHashKeySerializer(RedisSerializer.string());
+        redisTemplate.setValueSerializer(RedisSerializer.json());
+        redisTemplate.setHashValueSerializer(RedisSerializer.json());
+
+        //配置生效
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
