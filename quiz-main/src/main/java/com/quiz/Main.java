e5c839986e7004ca43acdb9aefbcfdf51864c429
乔治 <18890176107@163.com>
2024-04-15 13:09:44 +0800

    添加redis注解缓存配置

--- quiz-main/src/main/java/com/quiz/Main.java
+++ quiz-main/src/main/java/com/quiz/Main.java
@@ -3,9 +3,11 @@ package com.quiz;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan("com.quiz.mapper")
+@EnableCaching
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
--- quiz-main/src/main/java/com/quiz/config/RedisConfig.java
+++ quiz-main/src/main/java/com/quiz/config/RedisConfig.java
@@ -1,12 +1,16 @@
 package com.quiz.config;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.util.Objects;
 
 /**
  * <p>
@@ -31,5 +35,21 @@ public class RedisConfig {
         //配置生效
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
+    }
+
+    @Bean
+    public CacheManager redisCacheManager(RedisTemplate<String, Object> redisTemplate) {
+        final RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(redisTemplate.getStringSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(redisTemplate.getValueSerializer()))
+                .prefixCacheNameWith("quiz::");
+
+
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(Objects.requireNonNull(redisTemplate.getConnectionFactory()))
+                .cacheDefaults(config)
+                .build();
     }
 }
