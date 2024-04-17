b0c38b88bc8c03ea5a94c97874521515c893b736
乔治 <18890176107@163.com>
2024-04-17 22:07:50 +0800

    序列化时忽略password字段

--- quiz-main/src/main/java/com/quiz/entity/TUser.java
+++ quiz-main/src/main/java/com/quiz/entity/TUser.java
@@ -4,13 +4,15 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import java.io.Serializable;
-import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -45,6 +47,7 @@ public class TUser implements Serializable {
     @TableField("username")
     private String username;
 
+    @JsonIgnore
     @ApiModelProperty("密码")
     @TableField("password")
     private String password;
