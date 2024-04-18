196b4783e3d42a318111e38b38a1198574a04bcc
乔治 <18890176107@163.com>
2024-04-18 14:47:00 +0800

    包含用户权限

---
+++ quiz-main/src/main/java/com/quiz/dto/UserDto.java
@@ -0,0 +1,30 @@
+package com.quiz.dto;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.quiz.entity.TUser;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * <p>
+ * UserDto
+ * </p>
+ *
+ * @author XGeorge
+ * @since 2024/4/17 22:20
+ */
+@Getter
+@Setter
+@Accessors(chain = true)
+@ApiModel(value = "UserDto对象", description = "用户基本信息,添加权限列表")
+public class UserDto extends TUser implements Serializable {
+    @ApiModelProperty("用户权限集合")
+    @TableField("permissions")
+    private List<String> permissions;
+}
