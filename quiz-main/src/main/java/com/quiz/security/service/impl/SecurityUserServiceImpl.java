8c50e1ed8f922374e7ec273a9e458a67f06c9a7c
乔治 <18890176107@163.com>
2024-04-18 15:35:01 +0800

    修改SecurityUserServiceImpl的验证方式

--- quiz-main/src/main/java/com/quiz/security/service/impl/SecurityUserServiceImpl.java
+++ quiz-main/src/main/java/com/quiz/security/service/impl/SecurityUserServiceImpl.java
@@ -1,21 +1,14 @@
 package com.quiz.security.service.impl;
 
-import com.quiz.entity.TPermission;
-import com.quiz.entity.TUser;
-import com.quiz.mapper.TPermissionMapper;
-import com.quiz.service.ITUserService;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.quiz.dto.UserDto;
 import com.quiz.security.service.ISecurityUserService;
+import com.quiz.service.ITUserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -31,21 +24,20 @@ public class SecurityUserServiceImpl implements ISecurityUserService {
 
     private final ITUserService userService;
 
-    private final TPermissionMapper permissionMapper;
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         //根据账号获取用信息
-        TUser user = userService.getUserByAccount(username);
+        UserDto user = userService.getUserByAccount(username);
 
-        //根据用户id查询用户所有的权限
-        List<TPermission> permissions = permissionMapper.getUserPermissionsByUserId(user.getUserId());
+        // 如果走了缓存,就重新获取密码
+        if (StringUtils.isBlank(user.getPassword()))
+            user.setPassword(userService.getById(user.getUserId()).getPassword());
 
-        //生成 security 的权限
-        List<GrantedAuthority> grantedAuthorities = permissions.stream()
-                .filter(permission -> !Objects.isNull(permission))//不知道为什么size会为1,但是元素为null,所以先过滤
-                .map(permission -> new SimpleGrantedAuthority(permission.getPermissionName())).collect(Collectors.toList());
-        return new User(user.getUsername(), user.getPassword(), true, true
-                , true, user.getEnabled() >= 0, grantedAuthorities);
+        return User.builder()
+                .password(user.getPassword())
+                .username(user.getUsername())
+                .accountLocked(user.getEnabled() < 0)
+                .authorities(user.getPermissions().toArray(new String[0]))
+                .build();
     }
 }
