7a3c4a030040715f87e795b1de187fa898cc37ec
乔治 <18890176107@163.com>
2024-04-19 22:37:54 +0800

    feat: 添加項目常量

---
+++ quiz-main/src/main/java/com/quiz/constant/Constants.java
@@ -0,0 +1,14 @@
+package com.quiz.constant;
+
+/**
+ * <p>
+ * Constants 项目常量
+ * </p>
+ *
+ * @author XGeorge
+ * @since 2024/4/19 22:28
+ */
+public class Constants {
+    public static final String APP_NAME = "quiz";
+    public static final String WX_NAME = "WeChat";
+}
