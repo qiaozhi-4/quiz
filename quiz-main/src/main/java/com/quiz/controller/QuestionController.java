f43aae57d8b1b2c1958cab1cbe04ff3c59a46509
乔治 <18890176107@163.com>
2024-04-22 14:19:40 +0800

    🚩 添加接口,获取10个随机题目

--- quiz-main/src/main/java/com/quiz/controller/QuestionController.java
+++ quiz-main/src/main/java/com/quiz/controller/QuestionController.java
@@ -1,9 +1,16 @@
 package com.quiz.controller;
 
 
+import com.quiz.entity.Question;
+import com.quiz.mapper.QuestionMapper;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +23,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/question")
 @Api(tags = "题目表 前端控制器")
+@RequiredArgsConstructor
 public class QuestionController {
+    private final QuestionMapper questionMapper;
 
+    @GetMapping("get-random-questions")
+    @ApiOperation("随机获取10个题目")
+    public List<Question> getRandomQuestions() {
+        return questionMapper.selectRandomQuestions();
+    }
 }
--- quiz-main/src/main/java/com/quiz/mapper/QuestionMapper.java
+++ quiz-main/src/main/java/com/quiz/mapper/QuestionMapper.java
@@ -3,6 +3,8 @@ package com.quiz.mapper;
 import com.quiz.entity.Question;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
+import java.util.List;
+
 /**
  * <p>
  * 题目表 Mapper 接口
@@ -12,5 +14,9 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @since 2024-04-21  1024:23:35
  */
 public interface QuestionMapper extends BaseMapper<Question> {
+    /**
+     * 随机返回10个题目
+     */
+    List<Question> selectRandomQuestions();
 
 }
