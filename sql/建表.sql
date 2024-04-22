6d9bec99bb4a4b65cf7d67debf490016808f8bda
乔治 <18890176107@163.com>
2024-04-22 22:59:29 +0800

    🗃️ 修改表结构

--- sql/建表.sql
+++ sql/建表.sql
@@ -3,16 +3,16 @@ create database quiz;
 create table if not exists quiz.t_user
 (
     user_id       int unsigned auto_increment comment '主键，用户ID。',
-    nickname      varchar(64)      null comment '用户昵称',
-    avatar_url    varchar(255)     null comment '头像地址',
-    username      varchar(64)      null comment '用户账号',
-    password      varchar(64)      null comment '密码',
-    phone         varchar(32)      null comment '手机号',
-    mail          varchar(64)      null comment '邮箱',
-    created_at    datetime         not null comment '创建时间',
-    updated_at    datetime         null comment '更新时间',
-    last_login_at datetime         null comment '用户上次登录时间',
-    enabled       bigint default 0 not null comment '账号是否启用:0=正常,-1=禁用',
+    nickname      varchar(63)                        null comment '用户昵称',
+    avatar_url    varchar(255)                       null comment '头像地址',
+    username      varchar(63)                        null comment '用户账号',
+    password      varchar(63)                        null comment '密码',
+    phone         varchar(31)                        null comment '手机号',
+    mail          varchar(63)                        null comment '邮箱',
+    created_at    datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updated_at    datetime                           null comment '更新时间',
+    last_login_at datetime                           null comment '用户上次登录时间',
+    enabled       bigint   default 0                 not null comment '账号是否启用:0=正常,-1=禁用',
     primary key (user_id),
     INDEX username (username),
     INDEX phone (phone),
@@ -20,25 +20,25 @@ create table if not exists quiz.t_user
     INDEX username_phone_mail (username, phone, mail)
 ) comment '用户信息表';
 
-create table if not exists quiz.m_user_auth
+create table if not exists quiz.t_user_auth
 (
     auth_id     int unsigned auto_increment comment '主键，ID。',
     user_id     int unsigned not null comment '用户ID。',
-    provider    varchar(32)  not null comment '第三方服务的名称（如Google、Facebook等）。',
-    provider_id varchar(64)  not null comment '第三方提供的唯一标识符。比如微信的openId',
+    provider    varchar(31)  not null comment '第三方服务的名称（如Google、Facebook等）。',
+    provider_id varchar(63)  not null comment '第三方提供的唯一标识符。比如微信的openId',
     primary key (auth_id),
     INDEX user_id (user_id),
     INDEX provider_id (provider_id)
 ) comment '第三方登录表';
 
-create table if not exists quiz.m_role
+create table if not exists quiz.t_role
 (
     role_id   int auto_increment comment '主键，角色ID。',
     role_name varchar(255) not null comment '角色名称，如ROLE_ADMIN、ROLE_USER。',
     primary key (role_id)
 ) comment '角色表';
 
-create table if not exists quiz.m_permission
+create table if not exists quiz.t_permission
 (
     permission_id   int auto_increment comment '主键，权限ID。',
     permission_name varchar(255) not null comment '权限名称，如read、write。',
@@ -55,7 +55,7 @@ create table if not exists quiz.t_path
     INDEX permission_id (permission_id)
 ) comment '路径表';
 
-create table if not exists quiz.user_roles
+create table if not exists quiz.t_user_roles
 (
     user_id int not null comment '关联用户ID。',
     role_id int not null comment '关联角色ID。',
@@ -64,7 +64,7 @@ create table if not exists quiz.user_roles
     INDEX user_id_role_id (user_id, role_id)
 ) comment '用户角色关联表';
 
-create table if not exists quiz.role_permissions
+create table if not exists quiz.t_role_permissions
 (
     role_id       int not null comment '关联角色ID。',
     permission_id int not null comment '关联权限ID。',
@@ -73,39 +73,60 @@ create table if not exists quiz.role_permissions
     INDEX role_id_permission_id (role_id, permission_id)
 ) comment '角色权限关联表';
 
-create table quiz.t_question
+create table quiz.q_paper
+(
+    paper_id  int auto_increment comment '唯一主键',
+    user_id   int comment '出题用户id',
+    cover_url varchar(255) comment '封面url',
+    title     varchar(255) not null comment '标题',
+    describe_ varchar(511) comment '描述',
+    answers   varchar(63) comment '答案下标集合,以","分割',
+    primary key (paper_id),
+    INDEX user_id (user_id)
+) comment '题目试卷';
+
+create table quiz.q_tag
+(
+    tag_id   int auto_increment comment '唯一主键',
+    tag_name varchar(127) null comment '标签名称',
+    primary key (tag_id)
+) comment '试卷标签';
+
+create table quiz.q_question
 (
     question_id int auto_increment comment '唯一主键',
-    class_code  int                                null comment '类型代码',
-    title       varchar(512)                       not null comment '题目',
-    options_    varchar(1024)                      not null comment '选项',
+    title       varchar(511)                       not null comment '题目',
+    options_    varchar(1023)                      not null comment '选项',
     created_at  datetime default CURRENT_TIMESTAMP not null comment '创建时间',
     updated_at  datetime                           null comment '修改时间',
     primary key (question_id)
 ) comment '题目表';
 
-create table quiz.m_question_class
-(
-    class_id   int auto_increment comment '唯一主键',
-    class_code int          not null comment '类型代码',
-    class_name varchar(128) null comment '类型名称',
-    primary key (class_id)
-) comment '题目类别';
-
-create table quiz.question_paper
-(
-    paper_id  int auto_increment comment '唯一主键',
-    user_id   int comment '出题用户id',
-    questions varchar(256) not null comment '题目id集合,以","分割',
-    answers   varchar(64) comment '答案下标集合,以","分割',
-    primary key (paper_id)
-) comment '题目试卷';
-
-create table quiz.question_selects
+create table quiz.q_answers
 (
     answer_id int auto_increment comment '唯一主键',
     paper_id  int comment '关联试卷id',
     user_id   int comment '答题用户id',
-    selects   varchar(64) comment '选择下标集合,以","分割',
-    primary key (answer_id)
+    selects   varchar(63) comment '选择下标集合,以","分割',
+    primary key (answer_id),
+    INDEX paper_id (paper_id),
+    INDEX user_id (user_id)
 ) comment '题目答卷';
+
+create table quiz.q_paper_questions
+(
+    paper_id    int not null comment '试卷ID。',
+    question_id int not null comment '题目ID。',
+    INDEX paper_id (paper_id),
+    INDEX question_id (question_id),
+    INDEX paper_id_question_id (paper_id, question_id)
+) comment '试卷关联题目';
+
+create table quiz.q_paper_tags
+(
+    paper_id int not null comment '试卷ID。',
+    tag_id   int not null comment '标签ID。',
+    INDEX paper_id (paper_id),
+    INDEX tag_id (tag_id),
+    INDEX paper_id_tag_id (paper_id, tag_id)
+) comment '试卷关联标签';
