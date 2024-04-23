3d808b20eb084724d0e8ece36e4aef1cf7aadf51
乔治 <18890176107@163.com>
2024-04-23 11:44:16 +0800

    🏷️ 添加试卷相关内容

---
+++ quiz-main/src/main/java/com/quiz/service/IPaperService.java
@@ -0,0 +1,16 @@
+package com.quiz.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.quiz.entity.Paper;
+
+/**
+ * <p>
+ * 题目试卷 服务类
+ * </p>
+ *
+ * @author XGeorge
+ * @since 2024-04-23  1124:39:06
+ */
+public interface IPaperService extends IService<Paper> {
+
+}
---
+++ quiz-main/src/main/java/com/quiz/service/impl/PaperServiceImpl.java
@@ -0,0 +1,20 @@
+package com.quiz.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.quiz.entity.Paper;
+import com.quiz.mapper.PaperMapper;
+import com.quiz.service.IPaperService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 题目试卷 服务实现类
+ * </p>
+ *
+ * @author XGeorge
+ * @since 2024-04-23  1124:39:06
+ */
+@Service
+public class PaperServiceImpl extends ServiceImpl<PaperMapper, Paper> implements IPaperService {
+
+}
