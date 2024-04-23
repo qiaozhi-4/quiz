e9a5d7ac4d15d77cb38d78e447bc2218a8abc494
乔治 <18890176107@163.com>
2024-04-23 13:45:48 +0800

    🗃️ 修改插入脚本

--- sql/基本数据.sql
+++ sql/基本数据.sql
@@ -1,22 +1,22 @@
-INSERT INTO quiz.m_role (role_name)
+INSERT INTO quiz.t_role (role_name)
 VALUES ('ADMIN'),
        ('USER');
 
-INSERT INTO quiz.m_permission (permission_name)
+INSERT INTO quiz.t_permission (permission_name)
 VALUES ('CREATE'),
        ('READ'),
        ('UPDATE'),
        ('DELETE');
 
 -- ADMIN 角色权限
-INSERT INTO quiz.role_permissions (role_id, permission_id)
+INSERT INTO quiz.t_role_permissions (role_id, permission_id)
 VALUES (1, 1),
        (1, 2),
        (1, 3),
        (1, 4);
 
 -- USER 角色权限
-INSERT INTO quiz.role_permissions (role_id, permission_id)
+INSERT INTO quiz.t_role_permissions (role_id, permission_id)
 VALUES (2, 2);
 
 -- 添加测试用户
@@ -25,7 +25,7 @@ VALUES ('admin', '$2a$10$CyxrV5piFp1AdrNAzBlLHeGYCI1kr4ajrulkyJsZTlAeRz9.ENL7q',
        ('user', '$2a$10$qWlGYKJQF.Mv7P1KcmV8nuHSLF8HqcBT91/nts5h9h7lUtfHBFfwq', sysdate());
 
 -- 添加用户角色
-INSERT INTO quiz.user_roles (user_id, role_id)
+INSERT INTO quiz.t_user_roles (user_id, role_id)
 VALUES ('1', '1');
 
 -- 添加路径所需权限
