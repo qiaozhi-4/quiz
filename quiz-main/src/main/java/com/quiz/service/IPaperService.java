e603048f4b06f543c6b53f916c664e5318d9de87
乔治 <18890176107@163.com>
2024-04-24 16:41:01 +0800

    👔 添加方法插入试卷

--- quiz-main/src/main/java/com/quiz/service/IPaperService.java
+++ quiz-main/src/main/java/com/quiz/service/IPaperService.java
@@ -1,6 +1,7 @@
 package com.quiz.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.quiz.dto.PaperDto;
 import com.quiz.entity.Paper;
 
 /**
@@ -13,4 +14,10 @@ import com.quiz.entity.Paper;
  */
 public interface IPaperService extends IService<Paper> {
 
+    /**
+     * 保存试卷
+     *
+     * @param paperDto 试卷信息
+     */
+    Boolean savePaperInfo(PaperDto paperDto);
 }
--- quiz-main/src/main/java/com/quiz/service/impl/PaperServiceImpl.java
+++ quiz-main/src/main/java/com/quiz/service/impl/PaperServiceImpl.java
@@ -1,10 +1,19 @@
 package com.quiz.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.quiz.dto.PaperDto;
 import com.quiz.entity.Paper;
+import com.quiz.entity.PaperQuestions;
+import com.quiz.entity.PaperTags;
 import com.quiz.mapper.PaperMapper;
+import com.quiz.service.IPaperQuestionsService;
 import com.quiz.service.IPaperService;
+import com.quiz.service.IPaperTagsService;
+import com.quiz.utils.Assert;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -15,6 +24,30 @@ import org.springframework.stereotype.Service;
  * @since 2024-04-23  1124:39:06
  */
 @Service
+@RequiredArgsConstructor
 public class PaperServiceImpl extends ServiceImpl<PaperMapper, Paper> implements IPaperService {
 
+    private final IPaperQuestionsService paperQuestionsService;
+    private final IPaperTagsService paperTagsService;
+
+    @Override
+    public Boolean savePaperInfo(PaperDto paperDto) {
+        final Paper paper = PaperDto.convertFor(paperDto);
+        /* 插入试卷主体 */
+        Assert.isTrue(paper.insert(), "试卷插入失败");
+
+        /* 批量插入试卷关联题目数据 */
+        boolean pq = paperQuestionsService.saveBatch(paperDto.getQuestions().stream().map(question -> PaperQuestions.builder()
+                .paperId(paper.getPaperId())
+                .questionId(question.getQuestionId()).build()
+        ).collect(Collectors.toList()));
+        Assert.isTrue(pq, "试卷插入失败");
+
+        /* 批量插入试卷关联标签数据 */
+        boolean pt = paperTagsService.saveBatch(paperDto.getTags().stream().map(tag -> PaperTags.builder()
+                .paperId(paper.getPaperId())
+                .tagId(tag.getTagId()).build()).collect(Collectors.toList()));
+        Assert.isTrue(pt, "试卷插入失败");
+        return true;
+    }
 }
