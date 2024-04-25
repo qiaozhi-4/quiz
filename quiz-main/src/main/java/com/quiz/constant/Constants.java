5459aa0b0de2d6d99937bcfa8ec465dbee6aaf16
乔治 <18890176107@163.com>
2024-04-25 22:32:13 +0800

    🏷️ 添加分隔符常量

--- quiz-main/src/main/java/com/quiz/constant/Constants.java
+++ quiz-main/src/main/java/com/quiz/constant/Constants.java
@@ -12,4 +12,5 @@ public class Constants {
     public static final String APP_NAME = "quiz";
     public static final String WX_NAME = "WeChat";
     public static final String REDIS_WX_SESSION = "quiz::vx-session-key::";
+    public static final String SPACE_MARK = "@@";
 }
