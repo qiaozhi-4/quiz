c87b5a5814d65a23b0f9390d7386a40d16b323cd
乔治 <18890176107@163.com>
2024-04-26 16:26:36 +0800

    🔥 删除测试路径

--- quiz-main/src/main/java/com/quiz/controller/PermissionController.java
+++ quiz-main/src/main/java/com/quiz/controller/PermissionController.java
@@ -1,15 +1,10 @@
 package com.quiz.controller;
 
 
-import com.quiz.entity.Permission;
-import com.quiz.entity.User;
-import com.quiz.service.IPermissionService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Map;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
@@ -24,27 +19,4 @@ import java.util.Map;
 @Api(tags = "权限表 前端控制器")
 @RequiredArgsConstructor
 public class PermissionController {
-    private final IPermissionService permissionService;
-
-    @GetMapping("/test1")
-    public List<Permission> test1() {
-        return permissionService.list();
-    }
-
-    @GetMapping("/test2")
-    public List<Permission> tes2() {
-        throw new RuntimeException("测试异常");
-//        return permissionService.list();
-    }
-
-    @GetMapping("/test3/{current:\\d+}/{size:\\d+}")
-    public Map<String, Object> test3(@PathVariable("current") Integer current,
-                                     @PathVariable("size") Integer size) {
-        return permissionService.paging(current, size);
-    }
-
-    @PostMapping("test4")
-    public List<Permission> test4(@RequestBody User user){
-        return permissionService.list();
-    }
 }
