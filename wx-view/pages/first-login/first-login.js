5faece30c75850ae06988d08cc68aa0700520dd4
乔治 <18890176107@163.com>
2024-05-04 23:19:39 +0800

    🎨 头像和昵称基本样式完成

--- wx-view/pages/first-login/first-login.js
+++ wx-view/pages/first-login/first-login.js
@@ -1,9 +1,10 @@
 const app = getApp();
 Page({
   data: {
-    avatarUrl: '/images/avatar.svg',
-    avatarHeight: 71,
-    avatarWrapperBgColoc:'#7857D2'
+    avatarUrl: '/images/avatar-small.svg',
+    avatarHeight: 130,
+    avatarBgColoc: '#7857D2',
+    select: true
   },
   onChooseAvatar(e) {
     console.log('修改头像');
@@ -13,23 +14,9 @@ Page({
     this.setData({
       avatarUrl,
       avatarHeight: 125,
-      avatarWrapperBgColoc:'#fff'
+      avatarBgColoc: '#fff',
+      select: false
     });
     app.globalData.userInfo.avatarUrl = avatarUrl;
   },
-  onLoad() {
-    console.log('生命周期:onLoad-加载');
-  },
-  onShow() {
-    console.log('生命周期:onShow-显示');
-  },
-  onReady() {
-    console.log('生命周期:onReady');
-  },
-  onHide() {
-    console.log('生命周期:onHide-隐藏');
-  },
-  onUnload() {
-    console.log('生命周期:onUnload-卸载');
-  }
 });
