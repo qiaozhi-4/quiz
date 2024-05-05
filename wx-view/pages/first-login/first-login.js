a9e45105b48de98e7cfe13cf99a65aec80bbf6a6
乔治 <18890176107@163.com>
2024-05-05 12:39:07 +0800

    🎨 抖动动画添加改为微信动画

--- wx-view/pages/first-login/first-login.js
+++ wx-view/pages/first-login/first-login.js
@@ -15,7 +15,8 @@ Page({
       avatarUrl,
       avatarHeight: 125,
       avatarBgColoc: '#fff',
-      isSetAvatar: true
+      isSetAvatar: true,
+      animationShake: null
     });
     app.globalData.userInfo.avatarUrl = avatarUrl;
   },
@@ -26,11 +27,22 @@ Page({
     });
   },
   handleSubmitClick: function () {
-    this.setData({
-      shakeClass: 'shake-element'
+    // 创建动画实例
+    const animation = wx.createAnimation({
+      duration: 60, // 抖动持续时间为 0.6 秒
+      timingFunction: 'linear'
     });
+
+    // 应用抖动关键帧
+    animation.translateX(-10).step()
+      .translateX(10).step()
+      .translateX(-10).step()
+      .translateX(10).step()
+      .translateX(0).step();
+
+    // 更新数据，将动画传递到页面
     this.setData({
-      shakeClass: ''
+      animationShake: animation.export()
     });
   }
 });
