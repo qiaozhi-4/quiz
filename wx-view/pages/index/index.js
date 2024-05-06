540efa778207fcc3512a72fdafb56cf3373ec8bd
乔治 <18890176107@163.com>
2024-05-06 10:54:17 +0800

    🎨 添加登录跳转

--- wx-view/pages/index/index.js
+++ wx-view/pages/index/index.js
@@ -13,13 +13,28 @@ Page({
             method: 'POST',
             success(res) {
               console.log(res.data);
+              if (res.data.code != 200) {
+                console.log('登录失败!' + res.data.message);
+                return;
+              }
               var data = res.data.data;
-              if (data.userInfo.nickname == null) {
-                console.log('当前用户还没设置头像和用户名,跳转到设置页面');
-              }
-              else {
-                console.log('已设置头像和用户名');
-              }
+              app.globalData.userInfo = data.userInfo;
+              app.globalData.token = data.token;
+              /* 4秒后跳转页面 */
+              setTimeout(() => {
+                if (data.userInfo.nickname == null) {
+                  console.log('当前用户还没设置头像和用户名,跳转到设置页面');
+                  wx.redirectTo({
+                    url: '/pages/first-login/first-login',
+                  });
+                }
+                else {
+                  console.log('已设置头像和用户名');
+                  wx.redirectTo({
+                    url: '/pages/start-game/start-game',
+                  });
+                }
+              }, 4000);
             }
           });
         } else {
