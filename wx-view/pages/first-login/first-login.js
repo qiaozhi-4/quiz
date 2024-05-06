82b35dc1833c8661723454c6b2b745feaf16274f
乔治 <18890176107@163.com>
2024-05-06 10:54:53 +0800

    🎨 添加个人信息保存逻辑,以及跳转

--- wx-view/pages/first-login/first-login.js
+++ wx-view/pages/first-login/first-login.js
@@ -22,11 +22,16 @@ Page({
   },
   /* 设置昵称 */
   onChangeNickname(e) {
+    const { value } = e.detail;
     this.setData({
-      nicknameValue: e.detail.value
+      nicknameValue: value
     });
+    app.globalData.userInfo.nickname = value;
   },
   handleSubmitClick: function () {
+    console.log(app.globalData.userInfo);
+    console.log(app.globalData.token);
+    console.log(this.data);
     // 创建动画实例
     const animation = wx.createAnimation({
       duration: 60, // 抖动持续时间为 0.6 秒
@@ -44,5 +49,26 @@ Page({
     this.setData({
       animationShake: animation.export()
     });
+
+    const { nicknameValue, isSetAvatar } = this.data;
+    if (nicknameValue == '' || !isSetAvatar) return;
+    console.log('头像和昵称都设置了,保存个人信息');
+    wx.request({
+      url: 'https://qzwdyz.top/quiz/wx-user/save',
+      data: app.globalData.userInfo,
+      header: { token: app.globalData.token },
+      method: 'POST',
+      success(res) {
+        console.log(res.data);
+        if (res.data.code != 200) {
+          console.log('保存个人信息失败!' + res.data.message);
+          return;
+        }
+        wx.redirectTo({
+          url: '/pages/start-game/start-game',
+        });
+      }
+    });
+
   }
 });
