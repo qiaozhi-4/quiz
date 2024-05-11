d30f277f2213f3bc3aecf8f4770ae99465cba0a4
乔治 <18890176107@163.com>
2024-05-11 14:22:20 +0800

    🎨 优化代码结构，添加兼容性

--- wx-view/pages/start-game/start-game.js
+++ wx-view/pages/start-game/start-game.js
@@ -1,16 +1,14 @@
 const app = getApp();
 Page({
     data: {
-        date: new Date(Date.parse(new Date())+ 60*60*1000*8).toLocaleDateString(),
-        avatarUrl: app.globalData.userInfo.avatarUrl,
+        date: new Date(Date.parse(new Date()) + 60 * 60 * 1000 * 8).toLocaleDateString(),
+        avatarUrl: app.globalData.userInfo.avatarUrl ? app.globalData.userInfo.avatarUrl : '/images/avatar.svg',
         nickname: app.globalData.userInfo.nickname
-        // avatarUrl: 'https://img2.imgtp.com/2023/10/26/rQkEoyFQ.jpg',
-        // nickname:'乔治'
     },
     /* 点击开始路由到答题页 */
-    handleStartClick(){
+    handleStartClick() {
         wx.redirectTo({
             url: '/pages/answer/answer',
-          });
+        });
     },
 });
