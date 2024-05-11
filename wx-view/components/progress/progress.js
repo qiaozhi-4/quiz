c6a85188a4c8ffe73fce8bcfdf1d051c60179463
乔治 <18890176107@163.com>
2024-05-11 18:50:46 +0800

    💄 添加进度条组件

---
+++ wx-view/components/progress/progress.js
@@ -0,0 +1,40 @@
+Component({
+  behaviors: [],
+  properties: {
+    // 进度条宽度
+    strokeWidth: {
+      type: String,
+      value: '',
+    },
+    // 进度条高度
+    strokeHeight: {
+      type: String,
+      value: '',
+    },
+    // 进度条百分比
+    percentage: {
+      type: Number,
+      value: 10,
+    }
+  },
+  data: {
+
+  },
+  lifetimes: {
+    created() {
+
+    },
+    attached() {
+
+    },
+    moved() {
+
+    },
+    detached() {
+
+    },
+  },
+  methods: {
+
+  },
+});
