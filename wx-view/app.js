92c3c28cb3932ecf3e79705d578373d151afa941
乔治 <18890176107@163.com>
2024-05-11 11:28:52 +0800

    💄 添加样式

--- wx-view/app.js
+++ wx-view/app.js
@@ -2,21 +2,57 @@
 App({
   onLaunch() {
     // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    const logs = wx.getStorageSync('logs') || [];
+    logs.unshift(Date.now());
+    wx.setStorageSync('logs', logs);
 
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
-    })
+    });
+
+    // 获取胶囊信息
+    let menuButtonObject = wx.getMenuButtonBoundingClientRect();
+    console.log(menuButtonObject);
+    // 获取设备信息
+    wx.getSystemInfo({
+      success: res => {
+        console.log(res);
+
+        // 边距
+        let margin = menuButtonObject.top - res.statusBarHeight;
+        // 整个导航栏的高度
+        let navHeight = menuButtonObject.top + menuButtonObject.height + margin * 2;
+        this.globalData.navHeight = navHeight;
+        // 导航栏的高度
+        let nav = navHeight - res.statusBarHeight;
+        this.globalData.nav = nav;
+        // 导航栏距离顶部的高度
+        this.globalData.navBotHeight = menuButtonObject.top - margin;
+
+        // 胶囊与左边的距离
+        this.globalData.menuLeft = menuButtonObject.left;
+        // 胶囊的高度
+        this.globalData.menuHeight = menuButtonObject.height;
+        // 胶囊距离顶部的高度
+        this.globalData.menuBotHeight = margin;
+        // 胶囊距离右边的距离
+        this.globalData.menuBotRight = res.windowWidth - menuButtonObject.right;
+
+        // 整个设备的宽度
+        this.globalData.windowWidth = res.windowWidth;
+      },
+      fail: err => {
+        console.log(err);
+      }
+    });
   },
   globalData: {
     userInfo: {}
   },
-  
+
   onLoad() {
     console.log('生命周期:onLoad-加载');
   },
@@ -32,4 +68,4 @@ App({
   onUnload() {
     console.log('生命周期:onUnload-卸载');
   }
-})
+});
--- wx-view/pages/start-game/start-game.js
+++ wx-view/pages/start-game/start-game.js
@@ -1,9 +1,10 @@
 const app = getApp();
 Page({
     data: {
-        // avatarUrl: app.globalData.userInfo.avatarUrl,
-        // nickname: app.globalData.userInfo.nickname
-        avatarUrl: 'https://img2.imgtp.com/2023/10/26/rQkEoyFQ.jpg',
-        nickname:'乔治'
+        date: new Date(Date.parse(new Date())+ 60*60*1000*8).toLocaleDateString(),
+        avatarUrl: app.globalData.userInfo.avatarUrl,
+        nickname: app.globalData.userInfo.nickname
+        // avatarUrl: 'https://img2.imgtp.com/2023/10/26/rQkEoyFQ.jpg',
+        // nickname:'乔治'
     },
 });
