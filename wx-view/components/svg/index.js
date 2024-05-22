352a720aca2e4177c1dc294677de2af856557080
乔治 <18890176107@163.com>
2024-05-22 22:34:49 +0800

    ✨ 添加 stroke 属性支持

--- wx-view/components/svg/index.js
+++ wx-view/components/svg/index.js
@@ -13,20 +13,20 @@ Component({
                 success: res => {
                     let basestr = res.data
                     // 替换参数对应的值
-                    const fun = (parmName,paemVals) => {
+                    const fun = (parmName, paemVals) => {
                         let index = 0
                         basestr = basestr.replace(new RegExp(`${parmName}=".+?"`, 'g'), (word) => {
                             const newColor = paemVals[index]
                             index++
-                            return newColor ? `${parmName}="${newColor}"` : word
+                            return newColor ? word.replace(/".*"/,(w)=>`"${newColor}"`) : word
                         })
                     }
                     // 匹配颜色，并进行替换
                     if (colors && colors.length)
-                        fun('fill',colors)
+                        fun('(fill|stroke)', colors)
                     // 匹配透明度，并进行替换
                     if (opacitys && opacitys.length)
-                        fun('fill-opacity',opacitys)
+                        fun('(fill|stroke)-opacity', opacitys)
 
                     // 将 svg 数据进行 URL 编码
                     basestr = encodeURIComponent(basestr)
