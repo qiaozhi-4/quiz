088e816f9d33c7308130f797485b8a2e3051e50c
乔治 <18890176107@163.com>
2024-05-23 21:02:48 +0800

    💄 添加宽度高度属性

--- wx-view/components/svg/index.js
+++ wx-view/components/svg/index.js
@@ -1,6 +1,14 @@
 Component({
     properties: {
         src: String,
+        width: {
+            type: String,
+            value: '100%',
+        },
+        height: {
+            type: String,
+            value: '100%',
+        },
         colors: Array,
         opacitys: Array,
     },
@@ -12,13 +20,14 @@ Component({
                 encoding: 'binary',
                 success: res => {
                     let basestr = res.data
+                    console.log(basestr)
                     // 替换参数对应的值
                     const fun = (parmName, paemVals) => {
                         let index = 0
                         basestr = basestr.replace(new RegExp(`${parmName}=".+?"`, 'g'), (word) => {
                             const newColor = paemVals[index]
                             index++
-                            return newColor ? word.replace(/".*"/,(_)=>`"${newColor}"`) : word
+                            return newColor ? word.replace(/".*"/, (_) => `"${newColor}"`) : word
                         })
                     }
                     // 匹配颜色，并进行替换
@@ -28,6 +37,7 @@ Component({
                     if (opacitys && opacitys.length)
                         fun('(fill|stroke)-opacity', opacitys)
 
+                    console.log(basestr)
                     // 将 svg 数据进行 URL 编码
                     basestr = encodeURIComponent(basestr)
                     this.setData({
