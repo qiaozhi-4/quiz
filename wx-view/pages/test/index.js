eb133194a0889923d7604b953ebceccb14be0145
乔治 <18890176107@163.com>
2024-05-24 19:00:36 +0800

    💄 添加测试页

---
+++ wx-view/pages/test/index.js
@@ -0,0 +1,14 @@
+Page({
+    data: {
+        nbFrontColor: '#000000',
+        nbBackgroundColor: '#ffffff',
+    },
+    onLoad() {
+        this.setData({
+            nbTitle: '新标题',
+            nbLoading: true,
+            nbFrontColor: '#ffffff',
+            nbBackgroundColor: '#000000',
+        })
+    }
+})
