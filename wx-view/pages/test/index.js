0e5e279a644f7c4a8a5f7ff2dca665760169ecc0
乔治 <18890176107@163.com>
2024-05-25 19:22:54 +0800

    💄 完成基本样式

--- wx-view/pages/test/index.js
+++ wx-view/pages/test/index.js
@@ -1,14 +1,63 @@
+const app = getApp()
+const { request } = require('../../utils/service.js')
 Page({
     data: {
-        nbFrontColor: '#000000',
-        nbBackgroundColor: '#ffffff',
+        avatarUrl: app.globalData.userInfo?.avatarUrl,
+        questions: [], // 题目
+        options: [], // 选项
+        inputValues: [], // 输入框的值
+        index: 0, // 当前题目索引
+        activityContainer: '', // 活动预览窗口
     },
     onLoad() {
-        this.setData({
-            nbTitle: '新标题',
-            nbLoading: true,
-            nbFrontColor: '#ffffff',
-            nbBackgroundColor: '#000000',
+        // 获取随机题目
+        request({
+            // url: '/question/get-random-questions',
+            url: '/question/get-random-questions-test',
+            method: 'GET',
+        }).then((res) => {
+            wx.showToast({
+                title: '成功',
+                icon: 'success',
+                duration: 2000
+            })
+            this.setData({
+                questions: res.data
+            })
+        }).catch((err) => {
+            console.log('请求失败:' + err.message)
         })
-    }
+    },
+    // 题目变化触发
+    handleSwiperChange(e) {
+        this.setData({
+            index: e.detail.current,
+        })
+    },
+    // 点击选项触发
+    handleOptionClick(e) {
+        let index = this.data.index
+        let options = this.data.options
+        let option = e.target.dataset.option
+        options[index] = option
+        if (index >= this.data.questions.length - 1) return
+        this.setData({
+            index: index + 1,
+            options: options,
+        })
+    },
+    // 修改当前活动的容器
+    changeActivityContainer(e) {
+        let currentActivityContainer = this.data.activityContainer
+        let activity = e.target.dataset.activity
+        this.setData({
+            activityContainer: currentActivityContainer == activity ? '' : activity,
+        })
+    },
+    // 隐藏容器
+    hiddenContainer(e) {
+        this.setData({
+            activityContainer: ''
+        })
+    },
 })
