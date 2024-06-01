a393691fd3960a2e633e465311ff81292a7e9717
乔治 <18890176107@163.com>
2024-06-01 23:50:06 +0800

    🎨 封装一个request请求

---
+++ wx-view/miniprogram/utils/api.ts
@@ -0,0 +1,32 @@
+const app = getApp<IAppOption>()
+
+// 封装一个request请求
+const request = (options: WechatMiniprogram.RequestOption<Quiz.Result>) => {
+  // 返回一个promise对象
+  return new Promise((resolve, reject) => {
+    //设置基础请求头
+    const baseUrl = "https://qzwd yz.top/quiz"
+    wx.request<Quiz.Result>({
+      url: baseUrl + options.url, //仅为示例，并非真实的接口地址
+      data: options.data,
+      method: options.method,
+      header: {
+        'content-type': 'application/json', // 默认值
+        'token': app.globalData.token
+      },
+      success: (res) => {
+        console.log(res.data)
+        // 请求成功，就将成功的数据返回出去
+        if (res.data.code == 200) resolve(res.data)
+        else reject(res.data)
+      },
+      fail: (err) => {
+        console.error(err)
+        reject(err)
+      },
+    })
+  })
+}
+module.exports = {
+  request
+}
