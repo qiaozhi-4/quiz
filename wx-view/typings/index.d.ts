acdfb91f8b9df83f81a0e35f9d2ee89086683454
乔治 <18890176107@163.com>
2024-06-01 12:42:08 +0800

    🎨 给全局数据类型添加属性

--- wx-view/typings/index.d.ts
+++ wx-view/typings/index.d.ts
@@ -3,6 +3,7 @@
 interface IAppOption {
   globalData: {
     userInfo?: WechatMiniprogram.UserInfo,
+    token?: string,
   }
   userInfoReadyCallback?: WechatMiniprogram.GetUserInfoSuccessCallback,
 }
