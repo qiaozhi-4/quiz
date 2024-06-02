63b9c8c7f43f35ee98cab75e9921d199f9c61e13
乔治 <18890176107@163.com>
2024-06-02 18:05:22 +0800

    🎨 进入小程序时获取当前用户信息

--- wx-view/miniprogram/app.ts
+++ wx-view/miniprogram/app.ts
@@ -1,4 +1,6 @@
 // app.ts
+import { login } from "./utils/api"
+
 App<IAppOption>({
   globalData: {},
   onLaunch() {
@@ -10,8 +12,29 @@ App<IAppOption>({
     // 登录
     wx.login({
       success: res => {
-        console.log(res.code)
+        console.log('code: ', res.code)
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        if (res.code) {
+          // 发起网络请求
+          login(res.code).then((res) => {
+            this.globalData.userInfo = res.data.userInfo
+            this.globalData.token = res.data.token
+            if (res.data.userInfo.nickname == null) {
+              console.log('当前用户还没设置头像和用户名,跳转到设置页面')
+              wx.redirectTo({
+                url: '/pages/first-login/first-login',
+              })
+            }
+            else {
+              console.log('已设置头像和用户名')
+              wx.redirectTo({
+                url: '/pages/start-game/start-game',
+              })
+            }
+          })
+        } else {
+          console.error('登录失败！' + res.errMsg)
+        }
       },
     })
   },
