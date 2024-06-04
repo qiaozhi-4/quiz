8bfdc1841074105b5ab45e07e08ebd12f1e6a5bc
乔治 <18890176107@163.com>
2024-06-04 21:25:55 +0800

    💄 导航栏组件完成

--- wx-view/miniprogram/components/navigation-bar/navigation-bar.ts
+++ wx-view/miniprogram/components/navigation-bar/navigation-bar.ts
@@ -6,46 +6,42 @@ Component({
    * 组件的属性列表
    */
   properties: {
+    /** 额外的样式类 */
     extClass: {
       type: String,
       value: ''
     },
+    /** 导航栏标题 */
     title: {
       type: String,
       value: ''
     },
+    /** 背景颜色 */
     background: {
       type: String,
       value: ''
     },
+    /** 内容颜色 */
     color: {
       type: String,
       value: ''
     },
+    /** 返回上一页 */
     back: {
       type: Boolean,
       value: true
     },
-    loading: {
-      type: Boolean,
-      value: false
-    },
+    /** 返回主页 */
     homeButton: {
       type: Boolean,
       value: false,
     },
-    animated: {
-      // 显示隐藏的时候opacity动画效果
+    /** 显示加载中 */
+    loading: {
       type: Boolean,
-      value: true
+      value: false
     },
-    show: {
-      // 显示隐藏导航，隐藏的时候navigation-bar的高度占位还在
-      type: Boolean,
-      value: true,
-      observer: '_showChange'
-    },
-    // back为true的时候，返回的页面深度
+    /** back为true的时候，返回的页面深度 */
     delta: {
       type: Number,
       value: 1
@@ -55,21 +51,27 @@ Component({
    * 组件的初始数据
    */
   data: {
-    displayStyle: ''
+    /** 是否为IOS */
+    ios: false,
+    /** 导航高度 */
+    paddingAround: '',
+    /** 导航栏左右边距 */
+    height: '',
   },
+  // 组件生命周期声明对象
   lifetimes: {
     attached() {
       const rect = wx.getMenuButtonBoundingClientRect()
       wx.getSystemInfo({
         success: (res) => {
           const isAndroid = res.platform === 'android'
-          const isDevtools = res.platform === 'devtools'
           this.setData({
             ios: !isAndroid,
-            innerPaddingRight: `padding-right: ${res.windowWidth - rect.left}px`,
-            leftWidth: `width: ${res.windowWidth - rect.left }px`,
-            safeAreaTop: isDevtools || isAndroid ? `height: calc(var(--height) + ${res.safeArea.top}px); padding-top: ${res.safeArea.top}px` : ``
+            paddingAround: `padding-right: ${res.windowWidth - rect.right}px; padding-left: ${res.windowWidth - rect.right}px;`,
+            height: `height: calc(var(--padding) + ${rect.bottom}px);`,
           })
+          console.log('胶囊:', rect)
+          console.log('屏幕宽度:', res.windowWidth, ' 屏幕高度:', res.windowHeight)
         }
       })
     },
@@ -78,20 +80,7 @@ Component({
    * 组件的方法列表
    */
   methods: {
-    _showChange(show: boolean) {
-      const animated = this.data.animated
-      let displayStyle = ''
-      if (animated) {
-        displayStyle = `opacity: ${
-          show ? '1' : '0'
-        };transition:opacity 0.5s;`
-      } else {
-        displayStyle = `display: ${show ? '' : 'none'}`
-      }
-      this.setData({
-        displayStyle
-      })
-    },
+    /** 返回上一页 */
     back() {
       const data = this.data
       if (data.delta) {
@@ -99,7 +88,6 @@ Component({
           delta: data.delta
         })
       }
-      this.triggerEvent('back', { delta: data.delta }, {})
     }
   },
 })
