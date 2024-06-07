f83c4b1923e5dbcca6f203b7f7134e2fb4edb867
乔治 <18890176107@163.com>
2024-06-07 17:22:25 +0800

    自定义button组件

---
+++ wx-view/miniprogram/components/quiz-button/quiz-button.ts
@@ -0,0 +1,33 @@
+// components/quiz-button/quiz-button.ts
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    /** 宽度 */
+    width: {
+      type: String,
+      value: '105px',
+    },
+    /** 高度 */
+    height: {
+      type: String,
+      value: '40px',
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
