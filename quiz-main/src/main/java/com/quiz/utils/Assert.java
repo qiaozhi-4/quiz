a61e73beb0d2556734bdf053c6c4351093b9eeb7
乔治 <18890176107@163.com>
2024-06-07 12:33:34 +0800

    🎨 添加断言方法

--- quiz-main/src/main/java/com/quiz/utils/Assert.java
+++ quiz-main/src/main/java/com/quiz/utils/Assert.java
@@ -1,7 +1,10 @@
 package com.quiz.utils;
 
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.quiz.enumerate.ResultEnum;
 import com.quiz.exception.APIException;
+
+import java.util.Objects;
 
 /**
  * <p>
@@ -14,6 +17,16 @@ import com.quiz.exception.APIException;
 public class Assert {
     public static void isTrue(boolean expression, String message) {
         if (!expression)
+            throw APIException.builder().message(message).code(ResultEnum.FAILED.getCode()).build();
+    }
+
+    public static void isNotNull(String expression, String message) {
+        if (StringUtils.isBlank(expression))
+            throw APIException.builder().message(message).code(ResultEnum.FAILED.getCode()).build();
+    }
+
+    public static void isNotNull(Object expression, String message) {
+        if (Objects.isNull(expression))
             throw APIException.builder().message(message).code(ResultEnum.FAILED.getCode()).build();
     }
 }
