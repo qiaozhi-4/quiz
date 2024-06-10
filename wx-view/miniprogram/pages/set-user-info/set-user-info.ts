11b7fe06e873ed6ed8907005afe62f56d693d49a
乔治 <18890176107@163.com>
2024-06-10 20:25:17 +0800

    💄

--- wx-view/miniprogram/pages/set-user-info/set-user-info.ts
+++ wx-view/miniprogram/pages/set-user-info/set-user-info.ts
@@ -1,19 +1,61 @@
+import { userSave } from "../../utils/api"
+
 // pages/set-user-info/set-user-info.ts
+const app = getApp<IAppOption>()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    avatarUrl: '',
+    nickname: ''
   },
+  /* 设置头像 */
+  onChooseAvatar(e: WechatMiniprogram.TouchEvent) {
+    console.log(e.detail)
 
+    const { avatarUrl } = e.detail
+    if (!avatarUrl) return
+    this.setData({
+      avatarUrl,
+    })
+    /* 更新全局数据 */
+    app.globalData.userInfo.avatarUrl = avatarUrl
+  },
+  /* 设置昵称 */
+  onChangeNickname(e: WechatMiniprogram.TouchEvent) {
+    console.log(e.detail)
+
+    const { value } = e.detail
+    if (!value) return
+    this.setData({
+      nickname: value
+    })
+    /* 更新全局数据 */
+    app.globalData.userInfo.nickname = value
+  },
+  /* 保存个人信息到服务端 */
+  handleSubmitClick: function () {
+    console.log(app.globalData.userInfo)
+    console.log(app.globalData.token)
+
+    const { avatarUrl, nickname } = this.data
+    if (!avatarUrl || !nickname) return
+    userSave({ avatarUrl, nickname } as Quiz.UserInfo)
+      .then((res) => {
+        wx.redirectTo({
+          url: '/pages/index/index',
+        })
+      }).catch((err) => {
+        console.log('保存个人信息失败!' + err.data.message)
+      })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
-    console.log(options)
-    console.log(options.debug)
+  onLoad() {
 
   },
 
