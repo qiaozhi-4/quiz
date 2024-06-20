1b7a40f00786cafed86f8a31825922e7a39a0b16
乔治 <18890176107@163.com>
2024-06-20 17:26:38 +0800

    💄 添加答题完成页

--- wx-view/miniprogram/pages/index/index.ts
+++ wx-view/miniprogram/pages/index/index.ts
@@ -2,7 +2,7 @@
 
 Page({
   data: {
-    urls: ['set-user-info', 'start-game', 'answer',]
+    urls: ['set-user-info', 'start-game', 'answer', 'answer-finish',]
   },
   clickButton(e: WechatMiniprogram.TouchEvent) {
     let url = e.currentTarget.dataset.url
