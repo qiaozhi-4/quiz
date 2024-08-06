7f1bec7631642b66f4e2bafb5918ee1a27a3c35b
乔治 <18890176107@163.com>
2024-08-06 10:26:16 +0800

    🎨: 新增试卷API接口，优化试卷管理功能，增强系统灵活性。

---
+++ uni-min/utils/api/paper.ts
@@ -0,0 +1,34 @@
+import { request } from "../request"
+
+/** 保存试卷信息 */
+export function saveExam(paperDto : Quiz.Paper) {
+	return request({
+		url: `/paper/save-exam`,
+		method: 'POST',
+		data: paperDto,
+	})
+}
+
+/** 通过试卷ID,获取试卷详情 */
+export function get(paperId : number) {
+	return request({
+		url: `/paper/get/${paperId}`,
+		method: 'GET',
+	})
+}
+
+/** 获取试卷列表 */
+export function getAll() {
+	return request({
+		url: `/paper/get-all`,
+		method: 'GET',
+	})
+}
+
+/** 删除试卷 */
+export function delete_(paperId : number) {
+	return request({
+		url: `/paper/delete/${paperId}`,
+		method: 'DELETE',
+	})
+}
