d7670b92a58d033aa5fd86b0250d6b8c80b9226f
乔治 <18890176107@163.com>
2024-08-10 20:49:46 +0800

    🎨 优化日期时间处理，添加获取本周一时间的功能。

--- quiz-main/src/test/java/TestMain.java
+++ quiz-main/src/test/java/TestMain.java
@@ -1,5 +1,11 @@
+import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.AntPathMatcher;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
 
 /**
  * <p>
@@ -23,5 +29,17 @@ public class TestMain {
         final AntPathMatcher antPathMatcher = new AntPathMatcher();
         System.out.println(antPathMatcher.match("/permission/test3/1/1", "/permission/test3/{current:d+}/{size:d+}"));
         System.out.println(antPathMatcher.match("/permission/test3/{current:d+}/{size:d+}", "/permission/test3/1/1"));
+    }
+
+    /**
+     * 获取本周周一时间
+     */
+    @Test
+    public void testLocalDateTime() {
+        /* 获取本周周一 */
+        val localDateTime = LocalDateTime.now().with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY)).withHour(0).withMinute(0).withSecond(0).withNano(0);
+        /* 格式化 */
+        val format = localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.out.println("本周一的时间是：" + format);
     }
 }
