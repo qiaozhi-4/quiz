8001588b991ca50b1c131226f489682e8fddc625
乔治 <18890176107@163.com>
2024-08-12 15:07:55 +0800

    🎨 封装弹出框

--- uni-min/components/q-dialog/q-dialog.vue
+++ uni-min/components/q-dialog/q-dialog.vue
@@ -1,66 +1,55 @@
 <style lang="scss" scoped>
-	.confirm-dialog {
-		align-items: center;
-		
-		background-color: #1F1146;
-		padding: 20px;
-		border-radius: 5px;
-		box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+.q-dialog-mask {
+  position: absolute;
+  top: 0;
+  right: 0;
+  background-color: rgba(#000, 0.5);
+  width: 100vw;
+  height: 100vh;
+  z-index: 101;
 	}
 
-	.confirm-dialog-header {
-		font-weight: bold;
-		margin-bottom: 10px;
-	}
+.q-dialog {
+  width: 86vw;
 
-	.confirm-dialog-body {
-		margin-bottom: 20px;
-	}
+  background: #2F1969;
+  border-radius: 30px;
 
-	.confirm-dialog-footer {
-		display: flex;
-		justify-content: flex-end;
-	}
-
-	.confirm-dialog-footer button {
-		margin-left: 10px;
-		padding: 5px 10px;
-		border: none;
-		border-radius: 3px;
-		cursor: pointer;
-	}
-
-	.confirm-dialog-footer button:first-of-type {
-		background: #ccc;
-	}
-
-	.confirm-dialog-footer button:last-of-type {
-		background: #007bff;
-		color: white;
+  z-index: 102;
 	}
 </style>
 
 <template>
-	<page-container :show="props.show" round>
-		<view class="confirm-dialog flex-column">
-			<view class="confirm-dialog-header">
-				<slot name="header">确认操作</slot>
-			</view>
-			<view class="confirm-dialog-body">
-				<slot>你确定要执行此操作吗？</slot>
-			</view>
-			<view class="confirm-dialog-footer">
-				<button @click="$emit('onCancel')">取消</button>
-				<button @click="$emit('onConfirm')">确认</button>
-			</view>
-		</view>
-	</page-container>
+  <view v-if="showDialog">
+    <view class="q-dialog absolute-center ">
+      <slot></slot>
+    </view>
+    <!-- 点击遮罩层是否关闭提示框 -->
+    <view class="q-dialog-mask" @click="showDialog = !props.maskHideDialog">
+    </view>
+  </view>
 </template>
 
 <script lang="ts" setup>
-	import { ref, computed } from 'vue'
+import {ref} from 'vue'
+
+/** 传递的属性 */
 	const props = defineProps({
-		/** 标题文字 */
-		show: { type: Boolean, default: true },
+  /** 点击遮罩层是否关闭提示框 */
+  maskHideDialog: {type: Boolean, default: true},
+})
+
+/** 显示提示框 */
+const showDialog = ref(false)
+const show = () => {
+  console.log(show);
+  showDialog.value = true
+}
+const hide = () => {
+  console.log(hide);
+  showDialog.value = false
+}
+defineExpose({
+  show, hide
 	})
 </script>
