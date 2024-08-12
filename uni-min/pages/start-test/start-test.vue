297b55c4899e2588d9fa582a41f2c5833295ae68
乔治 <18890176107@163.com>
2024-08-12 16:00:04 +0800

    🎨 调整弹窗弹出条件

--- uni-min/pages/start-test/start-test.vue
+++ uni-min/pages/start-test/start-test.vue
@@ -95,137 +95,137 @@
 		}
 	}
 
-  .dialog {
-    padding: 28px;
-    gap: 12px;
-    align-items: center;
+	.dialog {
+		padding: 28px;
+		gap: 12px;
+		align-items: center;
 
-    .dialog-t1 {
-      font-family: 'Inter';
-      font-style: normal;
-      font-weight: 600;
-      font-size: 16px;
-      line-height: 19px;
-      text-align: center;
+		.dialog-t1 {
+			font-family: 'Inter';
+			font-style: normal;
+			font-weight: 600;
+			font-size: 16px;
+			line-height: 19px;
+			text-align: center;
 
-      color: rgba(255, 255, 255, 0.75);
-    }
+			color: rgba(255, 255, 255, 0.75);
+		}
 
-    .dialog-t2 {
-      font-family: 'Inter';
-      font-style: normal;
-      font-weight: 700;
-      font-size: 40px;
-      line-height: 48px;
-      text-align: center;
+		.dialog-t2 {
+			font-family: 'Inter';
+			font-style: normal;
+			font-weight: 700;
+			font-size: 40px;
+			line-height: 48px;
+			text-align: center;
 
-      color: #FFFFFF;
-    }
+			color: #FFFFFF;
+		}
 
-    .dialog-t3 {
-      font-family: 'Inter';
-      font-style: normal;
-      font-weight: 600;
-      font-size: 16px;
-      line-height: 19px;
+		.dialog-t3 {
+			font-family: 'Inter';
+			font-style: normal;
+			font-weight: 600;
+			font-size: 16px;
+			line-height: 19px;
 
-      color: rgba(255, 255, 255, 0.75);
-    }
+			color: rgba(255, 255, 255, 0.75);
+		}
 
-    .dialog-b1 {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      padding: 8px 10px;
-      gap: 10px;
+		.dialog-b1 {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			padding: 8px 10px;
+			gap: 10px;
 
-      width: 100%;
-      background: linear-gradient(90deg, #BE53FF 0%, #7756EC 100%);
-      border-radius: 15px;
+			width: 100%;
+			background: linear-gradient(90deg, #BE53FF 0%, #7756EC 100%);
+			border-radius: 15px;
 
-      .dialog-b1-svg {
-        position: relative;
+			.dialog-b1-svg {
+				position: relative;
 
-        .dialog-b1-svg-corner {
-          position: absolute;
-          top: 0;
-          right: 0;
-        }
+				.dialog-b1-svg-corner {
+					position: absolute;
+					top: 0;
+					right: 0;
+				}
 
-        .dialog-b1-svg-t1 {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          padding: 2px 4px;
+				.dialog-b1-svg-t1 {
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					padding: 2px 4px;
 
-          background: #FFFFFF;
-          border-radius: 15px;
-          font-family: 'Inter';
-          font-style: normal;
-          font-weight: 600;
-          font-size: 6px;
-          line-height: 7px;
+					background: #FFFFFF;
+					border-radius: 15px;
+					font-family: 'Inter';
+					font-style: normal;
+					font-weight: 600;
+					font-size: 6px;
+					line-height: 7px;
 
-          color: #A143FF;
-        }
-      }
+					color: #A143FF;
+				}
+			}
 
-      .dialog-b1-t1 {
-        font-family: 'Inter';
-        font-style: normal;
-        font-weight: 700;
-        font-size: 20px;
-        line-height: 24px;
+			.dialog-b1-t1 {
+				font-family: 'Inter';
+				font-style: normal;
+				font-weight: 700;
+				font-size: 20px;
+				line-height: 24px;
 
-        color: #FFFFFF;
-      }
-    }
+				color: #FFFFFF;
+			}
+		}
 
-    .dialog-b2 {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      padding: 8px 10px;
-      gap: 10px;
+		.dialog-b2 {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			padding: 8px 10px;
+			gap: 10px;
 
-      background: rgba(255, 255, 255, 0.1);
-      border-radius: 15px;
+			background: rgba(255, 255, 255, 0.1);
+			border-radius: 15px;
 
-      width: 100%;
+			width: 100%;
 
-      font-family: 'Inter';
-      font-style: normal;
-      font-weight: 700;
-      font-size: 20px;
-      line-height: 24px;
+			font-family: 'Inter';
+			font-style: normal;
+			font-weight: 700;
+			font-size: 20px;
+			line-height: 24px;
 
-      color: #FFFFFF;
-    }
-  }
+			color: #FFFFFF;
+		}
+	}
 </style>
 
 <template>
-  <!-- 弹出框 -->
-  <q-dialog ref="refDialog" :maskHideDialog="false">
-    <view class="dialog flex-column">
-      <text class="dialog-t1">复活宝石让尊贵的您获得重新再次答题的机会</text>
-      <text class="dialog-t2">复活宝石</text>
-      <q-image height="170" src="复活宝石" width="170"></q-image>
-      <text class="dialog-t3">失手选太快了吗？评分不满意？再答一次试试看！</text>
-      <button class="dialog-b1">
-        <view class="dialog-b1-svg">
-          <q-svg icon="复活宝石" size="34"/>
-          <q-svg v-if="resurrectionGem.value < 1" class="dialog-b1-svg-corner" icon="广告" size="11"/>
-          <text v-else class="dialog-b1-svg-corner dialog-b1-svg-t1">{{ resurrectionGem }}</text>
+	<!-- 弹出框 -->
+	<q-dialog ref="refDialog" :maskHideDialog="false">
+		<view class="dialog flex-column">
+			<text class="dialog-t1">复活宝石让尊贵的您获得重新再次答题的机会</text>
+			<text class="dialog-t2">复活宝石</text>
+			<q-image height="170" src="复活宝石" width="170"></q-image>
+			<text class="dialog-t3">失手选太快了吗？评分不满意？再答一次试试看！</text>
+			<button class="dialog-b1" @click="onResurrection">
+				<view class="dialog-b1-svg">
+					<q-svg icon="复活宝石" size="34" />
+					<q-svg v-if="resurrectionGem < 1" class="dialog-b1-svg-corner" icon="广告" size="11" />
+					<text v-else class="dialog-b1-svg-corner dialog-b1-svg-t1">{{ resurrectionGem }}</text>
 
-        </view>
-        <text class="dialog-b1-t1">使用宝石复活！</text>
-      </button>
-      <button class="dialog-b2">返回排行</button>
-    </view>
-  </q-dialog>
-  <!-- 提示消息 -->
-  <q-alert ref="refAlert"></q-alert>
+				</view>
+				<text class="dialog-b1-t1">使用宝石复活！</text>
+			</button>
+			<button class="dialog-b2" @click="goHome">返回排行</button>
+		</view>
+	</q-dialog>
+	<!-- 提示消息 -->
+	<q-alert ref="refAlert"></q-alert>
 	<view class="page">
 		<q-nav-bar></q-nav-bar>
 		<view class="main">
@@ -270,11 +270,12 @@ import {onMounted, ref} from 'vue'
 import {formatDate} from '../../utils/utils';
 import {onLoad} from '@dcloudio/uni-app'
 import {getUserById, verifyPaper} from '../../utils/api/user';
+import {gainProp} from '../../utils/api/prop';
 
 /** 对话框ref */
-  const refDialog = ref()
-  /** 提示消息ref */
-  const refAlert = ref()
+	const refDialog = ref()
+	/** 提示消息ref */
+	const refAlert = ref()
 	/** 获取登录信息 */
 	const userInfo = ref<Quiz.UserInfo>()
 	/** 朋友信息 */
@@ -285,10 +286,28 @@ import {getUserById, verifyPaper} from '../../utils/api/user';
 	const paperId = ref<number>()
 	/** 当前时间 */
 	const now = ref(formatDate(new Date, 'YYYY/MM/DD'));
-  /** 复活宝石数量 */
-  const resurrectionGem = ref<number>()
-
-
+	/** 复活宝石数量 */
+	const resurrectionGem = ref<number>()
+	/** 使用复活宝石 */
+	const onResurrection = () => {
+		if (resurrectionGem.value < 1) {
+			gainProp(userInfo.value.userId, 2, 1).then(res => {
+				refAlert.value.show({ msg: '假装你看完了视频,然后获取了宝石,并开始答题' })
+				setTimeout(() => {
+					uni.redirectTo({
+						url: `/pages/answer-test/answer-test?paperId=${paperId.value}&userId=${friendInfo.value.userId}`
+					});
+				}, 2000)
+			})
+		} else {
+			getApp().globalData.props.filter(e => e.propName == "复活宝石")[0].number -= 1
+			uni.redirectTo({
+				url: `/pages/answer-test/answer-test?paperId=${paperId.value}&userId=${friendInfo.value.userId}`
+			});
+		}
+	}
+	/** 回主页 */
+	const goHome = () => { uni.redirectTo({ url: `/pages/home/home` }) }
 	/** 点击'开始测试'跳转测试页 */
 	function onButton() {
 		if (isAnswer.value) {
@@ -303,10 +322,10 @@ import {getUserById, verifyPaper} from '../../utils/api/user';
 	}
 
 	onMounted(() => {
-    console.log('onMounted');
+		console.log('onMounted');
 	})
 	onLoad((option) => {
-    console.log('onLoad');
+		console.log('onLoad');
 		isAnswer.value = option.isAnswer === 'true'
 		if (isAnswer.value) {
 			paperId.value = option.paperId
@@ -317,15 +336,15 @@ import {getUserById, verifyPaper} from '../../utils/api/user';
 		/** 获取登录信息,直到获取成功 */
 		let intervalId = setInterval(() => {
 			userInfo.value = getApp().globalData.userInfo
-      resurrectionGem.value = getApp().globalData.props.filter(e => e.propName == "复活宝石")[0].number
 			if (userInfo.value) {
+				resurrectionGem.value = getApp().globalData.props.filter(e => e.propName == "复活宝石")[0].number
 				clearInterval(intervalId);
 				verifyPaper(userInfo.value.userId, paperId.value).then(res => {
 					if (res.data.isMyPaper) {
-            refAlert.value.show({msg: '不能回答自己的出题,2秒后返回主页'})
+						refAlert.value.show({ msg: '不能回答自己的出题,2秒后返回主页' })
 						setTimeout(() => uni.redirectTo({ url: `/pages/home/home` }), 2000)
-          } else if (res.data.isRepeatAnswers && resurrectionGem.value < 1) {
-            refDialog.value.show()
+					} else if (res.data.isRepeatAnswers) {
+						refDialog.value.show()
 					}
 				})
 			}
