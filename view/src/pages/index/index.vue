4279971bf7175fa659da4b02b1ad25f14beb5be1
乔治 <18890176107@163.com>
2024-08-18 10:09:22 +0800

    🎨 优化首页进度条，引入Q-Progress组件替换自定义进度条，优化UI效果

--- view/src/pages/index/index.vue
+++ view/src/pages/index/index.vue
@@ -7,9 +7,7 @@
         <!-- 底部 -->
         <view class="bottom flex-column">
             <!-- 进度条 -->
-            <view class="progress-wrap">
-                <view class="progress"></view>
-            </view>
+            <q-progress isRadius></q-progress>
             <text class="text">“我不关心你来自何处，我只关心你要去向何方”——Quiz</text>
         </view>
     </view>
@@ -36,42 +34,6 @@ const title = ref('Hello');
         justify-content: start;
         height: 145px;
         gap: 10px;
-
-        // 进度条
-        .progress-wrap,
-        .progress {
-            box-sizing: border-box;
-            border-radius: 15px;
-            border: 3px solid #FFFFFF;
-        }
-
-        .progress-wrap {
-            position: relative;
-            background: linear-gradient(90deg, #BE53FF 0%, #5830C7 100%);
-            height: 28px;
-            width: 90%;
-
-            .progress {
-                position: absolute;
-                background: #FFFFFF;
-                height: 100%;
-                top: 0;
-                left: 0;
-
-                animation: expandWidth 1s ease-in-out forwards;
-                /* 动画效果 */
-            }
-
-            @keyframes expandWidth {
-                from {
-                    width: 0;
-                }
-
-                to {
-                    width: 100%;
-                }
-            }
-        }
 
         .text {
             font-family: 'Inter';
