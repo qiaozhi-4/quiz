c03f17e337171eb92e25f8f9fbdeb5e4a347d67e
乔治 <18890176107@163.com>
2024-08-18 21:34:18 +0800

    🔧 优化store服务，添加新查询道具和修改道具的方法

--- view/src/stores/store.ts
+++ view/src/stores/store.ts
@@ -1,16 +1,25 @@
 import { defineStore } from "pinia";
 
 export const useStore = defineStore("counter", {
-  state: () => {
-    return {
-      /** 当前登录用户信息 */
-      user: {} as Quiz.UserDto,
-      /** 当前登录token */
-      token: "" as string,
-      /** 当前登录道具 */
-      props: [] as Quiz.PropDTO[],
-    };
-  },
-  getters: {},
-  actions: () => {},
+    state: () => {
+        return {
+            /** 当前登录用户信息 */
+            user: {} as Quiz.UserDto,
+            /** 当前登录token */
+            token: "" as string,
+            /** 当前登录道具 */
+            props: [] as Quiz.PropDTO[],
+        };
+    },
+    actions: {
+        /** 根据道具id获取道具 */
+        getPropById(propId: number) {
+            return this.props.find(e => e.propId === propId);
+        },
+        /** 根据道具id添加道具数量 */
+        addPropNumberById(propId: number, num: number) {
+            let prop = this.props.find(e => e.propId === propId);
+            if (prop) { prop.number += num; }
+        }
+    },
 });
