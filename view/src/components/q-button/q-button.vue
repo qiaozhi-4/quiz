164c9a82c3209fe5888c5f478ca52f93a602be3d
乔治 <18890176107@163.com>
2024-08-19 17:34:15 +0800

    添加按钮组件,未完善

---
+++ view/src/components/q-button/q-button.vue
@@ -0,0 +1,22 @@
+<template>
+    <view class="q-button text-overflow">
+        <slot>按钮</slot>
+    </view>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps();
+</script>
+
+<style lang="scss" scoped>
+.q-button {
+    height: 32px;
+}
+
+.q-button:active {
+    /* 缩小按钮 */
+    transform: scale(0.92);
+    /* 添加阴影以模拟发光效果 */
+    box-shadow: 0 0 15px rgba(98, 0, 234, 0.6);
+}
+</style>
