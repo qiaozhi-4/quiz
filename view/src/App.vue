474f8996f4375742a36b3d0d12dfb42cde420ec6
乔治 <18890176107@163.com>
2024-08-20 11:30:21 +0800

    🔧 修复登录逻辑，优化用户跳转，

--- view/src/App.vue
+++ view/src/App.vue
@@ -3,23 +3,24 @@ import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
 import { getPaperTotal } from "@/utils/api/paper";
 import { login } from "@/utils/api/wxUser";
 import { useStore } from "@/stores/store";
+import { objectToPathParams } from "@/utils/service";
 const store = useStore();
 onLaunch(() => {
     console.log("App Launch");
     uni.login({
         provider: "weixin", //使用微信登录
         success: (loginRes) => {
-            console.log("微信登录信息:", loginRes);
             login(loginRes.code, "wxf2f9d21291120320").then((res) => {
                 // login('wx0f4e873ad758a586', loginRes.code).then((res) => {
-                store.$patch(partialState =>{
-                    partialState.user = res.data.userInfo
-                    partialState.token = res.data.token
-                    partialState.props = res.data.props
-                })
+                store.$patch(partialState => {
+                    partialState.user = res.data.userInfo;
+                    partialState.token = res.data.token;
+                    partialState.props = res.data.props;
+                });
 
                 /** 获取第一个页面路由信息 */
                 let route = getCurrentPages()[0].route;
+
                 /** 判断是不是没有设置头像或者昵称 */
                 if (
                     res.data.userInfo.nickname == null ||
@@ -29,31 +30,28 @@ onLaunch(() => {
                 ) {
                     console.log("当前用户还没设置头像和用户名,跳转到设置页面");
                     if ("pages/index/index" == route) {
-                        uni.redirectTo({
-                            url: `/pages/set-user-info/set-user-info`,
+                        uni.reLaunch({
+                            url: `/pages/user-set/user-set`,
                         });
                     } else {
                         // 分享进来的要保留分享页
                         uni.navigateTo({
-                            url: `/pages/set-user-info/set-user-info`,
+                            url: `/pages/user-set/user-set`,
                         });
                     }
                 } else {
                     console.log("已设置头像和用户名");
-                    getPaperTotal(res.data.userInfo.userId).then((res) => {
-                        if ("pages/index/index" == route) {
-                            // 不是分享进来的就跳转
-                            if (res.data == 0) {
-                                uni.redirectTo({
-                                    url: `/pages/start-test/start-test?isAnswer=${false}`,
-                                });
-                            } else {
-                                uni.redirectTo({
-                                    url: `/pages/home/home`,
-                                });
-                            }
+                    if ("pages/index/index" == route) { // 不是分享进来的就跳转
+                        if (store.user.paperTotal == 0) {
+                            uni.reLaunch({
+                                url: `/pages/paper-start/paper-start`
+                            });
+                        } else {
+                            uni.reLaunch({
+                                url: `/pages/home/home`,
+                            });
                         }
-                    });
+                    }
                 }
             });
         },
