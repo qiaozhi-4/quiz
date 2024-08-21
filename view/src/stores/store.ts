f58886bd73c1ab32793fa8909b1756fcd4d646fa
乔治 <18890176107@163.com>
2024-08-21 09:24:44 +0800

    🎨 添加使用道具方法，优化功能逻辑

--- view/src/stores/store.ts
+++ view/src/stores/store.ts
@@ -13,13 +13,17 @@ export const useStore = defineStore("counter", {
     },
     actions: {
         /** 根据道具id获取道具 */
-        getPropById(propId: number): Quiz.PropDTO | undefined {            
+        getPropById(propId: number): Quiz.PropDTO | undefined {
             return this.props.find(e => e.propId == propId);
         },
         /** 根据道具id添加道具数量 */
         addPropNumberById(propId: number, num: number) {
             let prop = this.props.find(e => e.propId == propId);
             if (prop) { prop.number += num; }
+        },
+        /** 使用指定道具 */
+        usePropNumberById(propId: number, num = 1) {
+            this.addPropNumberById(propId, -num);
         }
     },
 });
